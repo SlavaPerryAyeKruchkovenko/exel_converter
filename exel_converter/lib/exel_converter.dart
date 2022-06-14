fbf4ad310b2d38547a21c0ed496c08c09b6576ed
Slava <sh-e-95@mail.ru>
2022-06-15 00:28:02 +0500

    base

--- exel_converter/lib/exel_converter.dart
+++ exel_converter/lib/exel_converter.dart
@@ -10,58 +10,5 @@ import 'models/schedule.dart';
 
 class Converter {
   /// Returns Schedule from plus 1.
-  Iterable<Schedule> convertToSchedule(File value) {
-    var data = value.readAsBytesSync();
-    List<Schedule> schedules = [];
-    var excel = Excel.decodeBytes(data);
-    var tables = excel.tables;
-    for (var key in tables.keys) {
-      if (tables[key] != null) {
-        var sheet = tables[key]!;
-        int groupCount = 0;
-        int start = "C".codeUnitAt(0);
-        try {
-          groupCount++;
-          start += 2;
-          schedules.add(_getSchedule(sheet, start));
-        } catch (ex) {
-          if (groupCount <= 0) {
-            throw Exception("$ex");
-          }
-        }
-      } else {
-        throw Exception('incorrect Excel list $key');
-      }
-    }
-    return schedules;
-  }
 
-  Schedule _getSchedule(
-    Sheet sheet,
-    int start,
-  ) {
-    var nameIndex = _getStartOfTable(sheet, String.fromCharCode(start));
-    var arr = sheet.cell(nameIndex).value.toString().split(' ');
-    var name = arr[0];
-    var groupNum = int.parse(arr[1]);
-    return Schedule(
-        schedules: _getSchedules(), groupName: name, groupNum: groupNum);
-  }
-
-  List<WeekSchedule> _getSchedules() {
-    throw Exception();
-  }
-
-  CellIndex _getStartOfTable(Sheet sheet, String letter) {
-    var maxIndex = 15;
-    CellIndex? cellIndex;
-    for (var i = 0; i <= maxIndex; i++) {
-      cellIndex = CellIndex.indexByString(letter + i.toString());
-      var cellArr = sheet.cell(cellIndex).value.toString().split(' ');
-      if (cellArr.length == 2 && int.tryParse(cellArr[1]) != null) {
-        return cellIndex;
-      }
-    }
-    throw Exception("haven't title of group");
-  }
 }
