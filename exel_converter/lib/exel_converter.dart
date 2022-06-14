37d5b5015afc00afaa691f7c1c97723568f3b894
Slava <sh-e-95@mail.ru>
2022-06-15 00:25:26 +0500

    create models

--- exel_converter/lib/exel_converter.dart
+++ exel_converter/lib/exel_converter.dart
@@ -2,7 +2,9 @@ library exel_converter;
 
 import 'dart:io';
 
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:excel/excel.dart';
+import 'package:exel_converter/models/week_schedule.dart';
 
 import 'models/schedule.dart';
 
@@ -10,15 +12,56 @@ class Converter {
   /// Returns Schedule from plus 1.
   Iterable<Schedule> convertToSchedule(File value) {
     var data = value.readAsBytesSync();
-
+    List<Schedule> schedules = [];
     var excel = Excel.decodeBytes(data);
     var tables = excel.tables;
     for (var key in tables.keys) {
       if (tables[key] != null) {
-        tables.cell(0);
+        var sheet = tables[key]!;
+        int groupCount = 0;
+        int start = "C".codeUnitAt(0);
+        try {
+          groupCount++;
+          start += 2;
+          schedules.add(_getSchedule(sheet, start));
+        } catch (ex) {
+          if (groupCount <= 0) {
+            throw Exception("$ex");
+          }
+        }
+      } else {
+        throw Exception('incorrect Excel list $key');
       }
     }
+    return schedules;
+  }
 
-    throw Exception("");
+  Schedule _getSchedule(
+    Sheet sheet,
+    int start,
+  ) {
+    var nameIndex = _getStartOfTable(sheet, String.fromCharCode(start));
+    var arr = sheet.cell(nameIndex).value.toString().split(' ');
+    var name = arr[0];
+    var groupNum = int.parse(arr[1]);
+    return Schedule(
+        schedules: _getSchedules(), groupName: name, groupNum: groupNum);
+  }
+
+  List<WeekSchedule> _getSchedules() {
+    throw Exception();
+  }
+
+  CellIndex _getStartOfTable(Sheet sheet, String letter) {
+    var maxIndex = 15;
+    CellIndex? cellIndex;
+    for (var i = 0; i <= maxIndex; i++) {
+      cellIndex = CellIndex.indexByString(letter + i.toString());
+      var cellArr = sheet.cell(cellIndex).value.toString().split(' ');
+      if (cellArr.length == 2 && int.tryParse(cellArr[1]) != null) {
+        return cellIndex;
+      }
+    }
+    throw Exception("haven't title of group");
   }
 }
--- exel_converter/lib/models/schedule.dart
+++ exel_converter/lib/models/schedule.dart
@@ -3,5 +3,9 @@ import 'week_schedule.dart';
 class Schedule {
   final List<WeekSchedule> schedules;
   final String groupName;
-  Schedule({required this.schedules, required this.groupName});
+  final int groupNum;
+  Schedule(
+      {required this.schedules,
+      required this.groupName,
+      required this.groupNum});
 }
