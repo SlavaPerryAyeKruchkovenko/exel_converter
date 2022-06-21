254dbdc3c19d4bdf1e6d38a08501b65557a5aa7a
Slava <sh-e-95@mail.ru>
2022-06-21 23:44:24 +0500

    add new model

---
+++ exel_converter/lib/models/day_schedule.dart
@@ -0,0 +1,7 @@
+import 'package:exel_converter/models/pair.dart';
+
+class DaySchedule {
+  final int dayNumber;
+  final List<Pair> pairs;
+  DaySchedule({required this.dayNumber, required this.pairs});
+}
